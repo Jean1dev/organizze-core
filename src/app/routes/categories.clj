48120d03df53d33986f33d91a3ffea94a59429a3
Jeanluca <jeanlucafp@gmail.com>
2025-08-21 17:05:19 -0300

    feat: add handlers for retrieving all categories and category by ID, update routes

--- src/app/routes/categories.clj
+++ src/app/routes/categories.clj
@@ -4,6 +4,7 @@
             [honey.sql :as sql]
             [io.pedestal.http.body-params :as body-params]
             [next.jdbc :as jdbc]
+            [next.jdbc.result-set :as rs]
             [schema.core :as s]))
 
 (s/defschema
@@ -20,11 +21,13 @@
    (response status nil))
   ([status body]
    (merge
-     {:status  status
+     {:status status
       :headers {"Content-Type" "application/json"}}
      (when body {:body (json/encode body)}))))
 
+(def ok (partial response 200))
 (def created (partial response 201))
+(def not-found (partial response 404))
 
 (def get-categorie-id!
   (fn [datasource uuid]
@@ -51,7 +54,40 @@
            id (save-categorie! ((:datasource dependencies)) categorie)]
        (assoc context :response (created {:id id}))))})
 
+(def get-all-categories-handler
+  {:name :get-all-categories-handler
+   :enter
+   (fn [{:keys [dependencies] :as context}]
+     (let [{:keys [datasource]} dependencies]
+       (let [select-query (sql/format {:select [:id :name :uuid :kind]
+                                       :from   :categories})
+             result (jdbc/execute! (datasource) select-query {:builder-fn rs/as-unqualified-kebab-maps})]
+         (assoc context :response (response 200 result)))))})
+
+(def get-by-id-categories-handler
+  {:name :get-by-id-categories-handler
+   :enter
+   (fn [{:keys [dependencies] :as context}]
+     (let [{:keys [datasource]} dependencies
+           categorie-id (-> context
+                             :request
+                             :path-params
+                             :categorie-id)
+           categorie (jdbc/execute-one!
+                  (datasource)
+                  (-> {:select :*
+                       :from   :categories
+                       :where  [:= :id categorie-id]}
+                      (sql/format))
+                  {:builder-fn rs/as-unqualified-kebab-maps})
+           response (if categorie
+                      (ok categorie)
+                      (not-found))]
+       (assoc context :response response)))})
+
 (def categories-routes
   #{
     ["/categories" :post [(body-params/body-params) post-categorie-handler] :route-name :post-categorie]
+    ["/categories" :get get-all-categories-handler :route-name :get-categories]
+    ["/categories/:categorie-id" :get get-by-id-categories-handler :route-name :get-by-id-categorie]
     })

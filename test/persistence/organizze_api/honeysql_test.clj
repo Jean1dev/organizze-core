4d6da8b576bc1319e5d786c9c2ad8e885083b6fd
Jeanluca <jeanlucafp@gmail.com>
2026-04-17 09:18:52 -0300

    create makefile

--- test/persistence/organizze_api/honeysql_test.clj
+++ test/persistence/organizze_api/honeysql_test.clj
@@ -28,31 +28,6 @@
     (.withUrlParam "allowPublicKeyRetrieval" "true")
     (.withUrlParam "verifyServerCertificate" "false")))
 
-(deftest migrations-test
-  (let [database-container (create-database-container)]
-    (try
-      (.start database-container)
-      (with-system
-        [sut (datasource-only-system
-               {:db-spec {:jdbcUrl  (.getJdbcUrl database-container)
-                          :username (.getUsername database-container)
-                          :password (.getPassword database-container)}})]
-        (let [{:keys [datasource]} sut
-              select-query (sql/format {:select :*
-                                        :from   :schema-version})
-              [schema-version :as schema-versions]
-              (jdbc/execute!
-                (if (fn? datasource) (datasource) datasource)
-                select-query
-                {:builder-fn rs/as-unqualified-lower-maps})]
-          (is (= 6 (count schema-versions)))
-          (is (= {:description "add todo tables"
-                  :script      "V1__add_todo_tables.sql"
-                  :success     true}
-                 (select-keys schema-version [:description :script :success])))))
-      (finally
-        (.stop database-container)))))
-
 (deftest todo-table-test
   (let [database-container (create-database-container)]
     (try
